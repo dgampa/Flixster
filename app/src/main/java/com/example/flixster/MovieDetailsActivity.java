b06650353f494bb16bb4402afdd5d57bc0ed44c6
Dhriti Gampa <dhritig@fb.com>
2022-06-03 15:04:51 -0700

    "finish"

--- app/src/main/java/com/example/flixster/MovieDetailsActivity.java
+++ app/src/main/java/com/example/flixster/MovieDetailsActivity.java
@@ -46,10 +46,10 @@ public class MovieDetailsActivity extends AppCompatActivity {
         String imageUrl;
         if (this.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
             imageUrl = movie.getBackdropPath();
-            //Glide.with(context).load(imageUrl).placeholder(R.drawable.flicks_backdrop_placeholder).into(ivPoster);
+            Glide.with(this).load(imageUrl).placeholder(R.drawable.flicks_backdrop_placeholder).into(ivPoster);
         } else {
             imageUrl = movie.getPosterPath();
-            //Glide.with(context).load(imageUrl).placeholder(R.drawable.flicks_movie_placeholder).into(ivPoster);
+            Glide.with(this).load(imageUrl).placeholder(R.drawable.flicks_movie_placeholder).into(ivPoster);
         }
         Glide.with(this).load(imageUrl).into(ivPoster);
 
